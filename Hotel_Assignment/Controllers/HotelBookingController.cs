bc09848209d7761bea775ea64fd9289d4f7fcd91
Durga <durgabhavani.moida@gmail.com>
2023-06-13 15:57:07 +0530

    Add project files.

---
+++ Hotel_Assignment/Controllers/HotelBookingController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Hotel_Assignment.Models;
+
+namespace Hotel_Assignment.Controllers
+{
+    public class HotelBookingController : Controller
+    { 
+         private static List<HotelBooking> _booking=new List<HotelBooking>();
+        public IActionResult HotelBookingDetails(HotelBooking booking)
+        {
+            _booking.Add(booking);
+            return Redirect(nameof(Index));
+            
+        }
+        public IActionResult Index() {
+           
+            return View(_booking);
+        }
+        public IActionResult Create()
+        {
+            var de = new HotelBooking();
+            return View(de);
+        }
+    }
+}
---
+++ Hotel_Assignment/Models/HotelBooking.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotel_Assignment.Models
+{
+    public class HotelBooking
+    {
+        public int Id { get; set; }
+        public string GuestName { get; set; } = String.Empty;
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateEnd { get; set; } 
+       
+    }
+}
